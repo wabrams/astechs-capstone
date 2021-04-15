770c32113b77ea90f81adc6d0591632aec3d1c9e
Will Abrams <wabrams@users.noreply.github.com>
2021-04-14 20:47:01 -0600

    refactor WebUI

---
+++ hub/app.js
@@ -0,0 +1,107 @@
+// File: webserver.js
+// Authors: Malcolm McKellips, William Abrams
+// Description: Backend server for servicing Astechs Web UI
+
+// Server Setup
+var http = require('http');
+var server = http.createServer(server_handler);
+var io = require('socket.io')(server)
+
+// File Setup
+var fs = require('fs'); //require filesystem module
+
+// // Serial Port Setup
+// const SerialPort = require('serialport');
+// const Readline = require('@serialport/parser-readline');
+// const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
+// const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+server.listen(8080); 
+console.log('[SETUP]: listening on *:8080')
+
+//Create Server
+var count = 0;
+function server_handler (req, res)
+{
+  console.log("[REQ]: " + req.url);
+  count += 1;
+  if (req.url == '/index.html' || req.url == "/" || req.url == "/:")
+  {
+    fs.readFile(__dirname + '/html/index.html', function(err, text)
+    {
+      res.setHeader("Content-Type", "text/html");
+      res.end(text);
+    });
+  }
+  else if (req.url == '/css/basic.css')
+  {
+    fs.readFile("css/basic.css", function (err, text)
+    {
+      res.setHeader("Content-Type", "text/css");
+      res.end(text);
+    })
+  }
+  else if (req.url == '/favicon.ico')
+  {
+    fs.readFile("img/favicon-32x32.png", function (err, text)
+    {
+      res.setHeader("Content-Type", "image/png");
+      res.end(text);
+    })
+  }
+  else if (req.url == '/img/node_on.png')
+  {
+    fs.readFile("img/node_on.png", function (err, text)
+    {
+      res.setHeader("Content-Type", "image/png");
+      res.end(text);
+    })
+  }
+  else if (req.url == '/img/node_off.png')
+  {
+    fs.readFile("img/node_off.png", function (err, text)
+    {
+      res.setHeader("Content-Type", "image/png");
+      res.end(text);
+    })
+  }
+  else if (req.url == "/img/astechs.png")
+  {
+    fs.readFile("img/astechs.png", function (err, text)
+    {
+      res.setHeader("Content-Type", "image/png");
+      res.end(text);
+    })
+  }
+  else if (req.url == "/js/threadParse.js")
+  {
+    fs.readFile("js/threadParse.js", function (err, text)
+    {
+      res.setHeader("Content-Type", "text/js");
+      res.end(text);
+    })
+  }
+  else if (req.url == "/js/powerPlot.js")
+  {
+    fs.readFile("js/powerPlot.js", function (err, text)
+    {
+      res.setHeader("Content-Type", "text/js");
+      res.end(text);
+    })
+  }
+};
+
+// WebSocket Connection
+io.on('connection', (socket) => 
+{
+  console.log('a user connected');
+  socket.on('disconnect', () => 
+  {
+    console.log('user disconnected');
+  });
+  socket.on('updateReq', () => 
+  {
+    console.log('[SIO]: update request received');
+    io.emit('updateRes', count);
+  });
+});
